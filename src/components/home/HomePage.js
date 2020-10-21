d963cc7d3d51151179af51966e517816b28fec53
0surface <tms.0xvoid@gmail.com>
2020-10-21 12:33:24 +0100

    changed,show favicon with attribution

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,8 +3,24 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => (
   <div className="jumbotron">
+    <img src="../favicon.svg" height="100" width="100"></img>
     <h1>LightHouse Manager</h1>
     <p>React, Redux and React Router for ultra-responsive web apps.</p>
+
+    <div>
+      <small className="text-muted">
+        Icons made by{" "}
+        <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">
+          Good Ware
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          {" "}
+          www.flaticon.com
+        </a>
+      </small>
+    </div>
+    <br />
     <Link to="about" className="btn btn-primary btn-lg">
       Learn more
     </Link>
