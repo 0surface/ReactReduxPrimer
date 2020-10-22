21046145de02487a6421de0c7bac37ca09a7fe08
0surface <tms.0xvoid@gmail.com>
2020-10-22 10:03:04 +0100

    create redux store configuration

---
+++ src/redux/configureStore.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+
+export default function configureStore(initialState) {
+  //add sipport for Redux dev tools
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+  );
+  /* reduxImmutableStateInvariant
+  will warn us if we accidentally mutate Reduc state */
+}
